f09495a23baa824823417014a96ebbbf2302bf05
wjdru <wjdrudtn0225@naver.com>
2018-05-14 18:10:56 +0900

    Initial commit

---
+++ InputTest.ino
@@ -0,0 +1,17 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13,OUTPUT);
+  pinMode(2,INPUT_PULLUP);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int val = digitalRead(2);
+  if( val == HIGH){
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  }
+
+}
